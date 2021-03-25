5aa9339bcb72b0a0ed1eb780f9ea1bd0bb39d3e0
Michael Condouris <condour@gmail.com>
2021-03-25 08:44:33 -0400

    indicator code

--- indicator/indicator.ino
+++ indicator/indicator.ino
@@ -20,38 +20,65 @@ unsigned long lastTime = 0;
 // Timer set to 10 minutes (600000)
 //unsigned long timerDelay = 600000;
 // Set timer to 5 seconds (5000)
-unsigned long timerDelay = 5000;
-
+unsigned long timerDelay = 10000;
+time_t now;
+char strftime_buf[64];
+struct tm timeinfo;
 // Replace with your network credentials
 const char* ssid = "condourama";
 const char* checkEndpoint = "http://192.168.1.66/";
 const char* toggleEndpoint = "http://192.168.1.66/toggle";
-const int openPin = 13;
+const int openPin = 33;
 const int closedPin = 12;
-const int networkPin = 14;
-const int buttonPin = 15;
+const int networkPin = 23;
 int connectCount = 0;
 String fetchedResult;
 struct Button {
   const uint8_t PIN;
   uint32_t numberKeyPresses;
   bool pressed;
+  uint32_t lastTime;
 };
 
-Button button1 = {18, 0, false};
+TaskHandle_t Task1;
 
-void IRAM_ATTR isr() {
-  button1.numberKeyPresses += 1;
-  button1.pressed = true;
+Button button1 = {18, 0, false, millis()};
+//
+//void IRAM_ATTR isr() {
+//  Serial.println("IN INTERRUPT");
+//  button1.numberKeyPresses += 1;
+//  button1.lastTime = millis();
+//  button1.pressed = true;
+//}
+void Task1code( void * parameter) {
+  for(;;) {
+    if(digitalRead(button1.PIN) == LOW){
+      button1.numberKeyPresses++;
+      if(button1.numberKeyPresses > 3) {
+        button1.pressed = true;
+      }
+    } else {
+      if(button1.numberKeyPresses > 0) {
+        button1.numberKeyPresses--;
+      }
+    }
+    delay(100);
+  }
 }
 
 void setup() {
+    xTaskCreatePinnedToCore(
+                    Task1code,   /* Task function. */
+                    "Task1",     /* name of task. */
+                    10000,       /* Stack size of task */
+                    NULL,        /* parameter of the task */
+                    1,           /* priority of the task */
+                    &Task1,      /* Task handle to keep track of created task */
+                    1); 
   Serial.begin(115200);
   pinMode(button1.PIN, INPUT_PULLUP);
-  attachInterrupt(button1.PIN, isr, FALLING);
   pinMode(openPin, OUTPUT);
   pinMode(closedPin, OUTPUT);
-  pinMode(buttonPin, INPUT);
   Serial.println(WiFi.macAddress());
 
   // Connect to Wi-Fi network with SSID and password
@@ -60,18 +87,21 @@ void setup() {
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     digitalWrite(networkPin, HIGH);
+    Serial.println("should be toggling HIGH");
     delay(250);
     connectCount++;
     digitalWrite(networkPin, LOW);
+        Serial.println("should be toggling LOW");
     delay(250);
     Serial.println("not yet...");
     Serial.println(WiFi.status());
-    if(connectCount > 10) {
+    if (connectCount > 10) {
       digitalWrite(networkPin, HIGH);
       WiFi.begin(ssid, password);
+      
       connectCount = 0;
       delay(500);
-      
+
       digitalWrite(networkPin, LOW);
     }
   }
@@ -86,16 +116,16 @@ void setup() {
 
 String httpGETRequest(const char* serverName) {
   HTTPClient http;
-    
-  // Your IP address with path or Domain name with URL path 
+
+  // Your IP address with path or Domain name with URL path
   http.begin(serverName);
-  
+
   // Send HTTP POST request
   int httpResponseCode = http.GET();
-  
-  String payload = "{}"; 
-  
-  if (httpResponseCode>0) {
+
+  String payload = "{}";
+
+  if (httpResponseCode > 0) {
     payload = http.getString();
   }
   else {
@@ -108,32 +138,36 @@ String httpGETRequest(const char* serverName) {
   return payload;
 }
 void loop() {
-  
-  //Send an HTTP POST request every 10 minutes
+
+  //Send an HTTP POST request every 5 sec
   if ((millis() - lastTime) > timerDelay) {
-      if (button1.pressed) {
+    if (button1.pressed) {
+      
+    
       Serial.printf("Button 1 has been pressed %u times\n", button1.numberKeyPresses);
+//      Serial.println(timeClient.getFormattedTime());
+      Serial.println("Issuing toggle");
+    
       httpGETRequest(toggleEndpoint);
       button1.pressed = false;
-  }
+      button1.numberKeyPresses = 0;
+    
+    }
     //Check WiFi connection status
-    if(WiFi.status()== WL_CONNECTED){
-  
+    if (WiFi.status() == WL_CONNECTED) {
+
       // Your Domain name with URL path or IP address with path
       fetchedResult = httpGETRequest(checkEndpoint);
       // Send HTTP GET request
       JSONVar resultJSON = JSON.parse(fetchedResult);
-      if(JSON.typeof(resultJSON) == "undefined") {
+      if (JSON.typeof(resultJSON) == "undefined") {
         Serial.println("PARSE FAILURE");
         return;
       }
-      if((resultJSON["status"] == (JSONVar) "Closed")){
-        Serial.println("CLOSED");
+      if ((resultJSON["status"] == (JSONVar) "Closed")) {
         digitalWrite(closedPin, HIGH);
         digitalWrite(openPin, LOW);
       } else {
-        Serial.println((const char*) resultJSON["status"]);
-        Serial.println("OPEN");
         digitalWrite(openPin, HIGH);
         digitalWrite(closedPin, LOW);
       }
